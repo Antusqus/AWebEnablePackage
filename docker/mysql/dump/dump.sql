9a32b3402b194345456974ab22da59f09987c3a1
Anthony Quach <ta.quach@hotmail.com>
2025-10-10 13:03:09 +0200

    docker setup

--- docker/mysql/dump/dump.sql
+++ docker/mysql/dump/dump.sql
@@ -3,3 +3,10 @@ CREATE TABLE TestPotato (
     Roots VARCHAR(255),
     Season VARCHAR(255)
 );
+
+CREATE TABLE Users (
+    FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+    Email VARCHAR(255),
+    Password VARCHAR(255)
+);
